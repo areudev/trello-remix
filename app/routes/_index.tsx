a46d63c5f1209f00e96de4a16a28f48905aa9c04
Chris Lime <chrkalivas@gmail.com>
2024-01-13 00:56:37 +0200

    Add tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {MetaFunction} from '@remix-run/node'
+// import App from "~/root";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    {title: 'New Remix App'},
+    {name: 'description', content: 'Welcome to Remix!'},
+  ]
+}
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.8'}}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
           <a
             target="_blank"
             href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             15m Quickstart Blog Tutorial
           </a>
         </li>
@@ -25,8 +25,7 @@ export default function Index() {
           <a
             target="_blank"
             href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             Deep Dive Jokes App Tutorial
           </a>
         </li>
@@ -37,5 +36,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  )
 }
