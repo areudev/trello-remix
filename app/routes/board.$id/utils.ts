b2388e7c1ce892abe989f6862ee67b737f48f697
Chris Lime <chrkalivas@gmail.com>
2024-01-14 12:25:12 +0200

    Add new files and update dependencies

---
+++ app/routes/board.$id/utils.ts
@@ -0,0 +1,18 @@
+import invariant from 'tiny-invariant'
+import {ItemMutation, ItemMutationFields} from './types'
+
+export function parseItemMutation(formData: FormData): ItemMutation {
+  const id = ItemMutationFields.id.type(formData.get('id'))
+  invariant(id, 'Missing item id')
+
+  const columnId = ItemMutationFields.columnId.type(formData.get('columnId'))
+  invariant(columnId, 'Missing column id')
+
+  const order = ItemMutationFields.order.type(formData.get('order'))
+  invariant(typeof order === 'number', 'Missing order')
+
+  const title = ItemMutationFields.title.type(formData.get('title'))
+  invariant(title, 'Missing title')
+
+  return {id, columnId, order, title}
+}
